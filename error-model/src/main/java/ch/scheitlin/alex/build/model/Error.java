b16679e5964f49604ede72f4ffe8c9f216926d28
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-14 11:15:27 +0200

    Add java doc to error-model

--- error-model/src/main/java/ch/scheitlin/alex/build/model/Error.java
+++ error-model/src/main/java/ch/scheitlin/alex/build/model/Error.java
@@ -1,12 +1,43 @@
 package ch.scheitlin.alex.build.model;
 
+/**
+ * Represents an error occurred in a particular file.
+ */
 public class Error {
+    /**
+     * The path (directory) of the file where the error occurred.
+     */
     private String path;
+
+    /**
+     * The name (and file ending) of the file where the error occurred.
+     */
     private String file;
+
+    /**
+     * The line within the file where the error occurred.
+     */
     private int line;
+
+    /**
+     * The column within the file where the error occurred.
+     */
     private int column;
+
+    /**
+     * The error message describing the error.
+     */
     private String message;
 
+    /**
+     * Creates a new instance of an error.
+     *
+     * @param path    the path (directory) of the file where the error occurred
+     * @param file    the name (and file ending) of the file where the error occurred
+     * @param line    the line within the file where the error occurred
+     * @param column  the column within the file where the error occurred
+     * @param message the error message describing the error
+     */
     public Error(String path, String file, int line, int column, String message) {
         this.path = path;
         this.file = file;
@@ -15,22 +46,47 @@ public class Error {
         this.message = message;
     }
 
+    /**
+     * Gets the path (directory) of the file where the error occurred.
+     *
+     * @return the path (directory) of the file where the error occurred
+     */
     public String getPath() {
         return this.path;
     }
 
+    /**
+     * Gets the name (and file ending) of the file where the error occurred.
+     *
+     * @return the name (and file ending) of the file where the error occurred
+     */
     public String getFile() {
         return this.file;
     }
 
+    /**
+     * Gets the line within the file where the error occurred.
+     *
+     * @return the line within the file where the error occurred
+     */
     public int getLine() {
         return this.line;
     }
 
+    /**
+     * Gets the column within the file where the error occurred.
+     *
+     * @return the column within the file where the error occurred
+     */
     public int getColumn() {
         return this.column;
     }
 
+    /**
+     * Gets the error message describing the error.
+     *
+     * @return the error message describing the error
+     */
     public String getMessage() {
         return this.message;
     }
