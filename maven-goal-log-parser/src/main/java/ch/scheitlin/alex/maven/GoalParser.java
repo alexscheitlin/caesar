fd6763e8c7ffabc8038895cebfa06762a5c05acf
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-14 17:30:52 +0200

    Refine maven-goal-log-parser to also parse error messages

--- maven-goal-log-parser/src/main/java/ch/scheitlin/alex/maven/GoalParser.java
+++ maven-goal-log-parser/src/main/java/ch/scheitlin/alex/maven/GoalParser.java
@@ -7,21 +7,46 @@ import java.util.List;
 public abstract class GoalParser {
     private String PLUGIN_NAME;
     private String GOAL_NAME;
+    private String ERROR_MESSAGE;
 
     protected GoalParser(String pluginName, String goalName) {
         this.PLUGIN_NAME = pluginName;
         this.GOAL_NAME = goalName;
     }
 
+    protected GoalParser(String errorMessage) {
+        this.ERROR_MESSAGE = errorMessage;
+    }
+
     /**
-     * Checks whether the parser is able to parse the a log of the specified goal of the specified plugin or not.
+     * Checks whether the parser is able to parse the log of the specified goal of the specified plugin or not.
      *
      * @param plugin the name of the plugin of the goal to parse the log
-     * @param goal the name of the goal to parse the log
-     * @return
+     * @param goal   the name of the goal to parse the log
+     * @return {@code true} if this parser is able to parse the specified goal of the specified plugin
+     * or {@code false} if not
      */
-    public boolean isParserFor(String plugin, String goal) {
+    public boolean isParserForPlugin(String plugin, String goal) {
+        if (PLUGIN_NAME == null || GOAL_NAME == null) {
+            return false;
+        }
+
         return plugin.equals(PLUGIN_NAME) && goal.equals(GOAL_NAME);
+    }
+
+    /**
+     * Checks whether the parser is able to parse the log of the specified error message.
+     *
+     * @param message the error message
+     * @return {@code true} if this parser is able to parse the specified error message
+     * or {@code false} if not
+     */
+    public boolean isParserForErrorMessage(String message) {
+        if (ERROR_MESSAGE == null) {
+            return false;
+        }
+
+        return message.equals(ERROR_MESSAGE);
     }
 
     public abstract List<Error> parseLog(String log);
--- maven-goal-log-parser/src/main/java/ch/scheitlin/alex/maven/MavenGoalLogParser.java
+++ maven-goal-log-parser/src/main/java/ch/scheitlin/alex/maven/MavenGoalLogParser.java
@@ -22,8 +22,8 @@ public class MavenGoalLogParser {
      * Tries to parse the log of a goal with one of the available goal parsers.
      *
      * @param pluginName the plugin name of the log to parse
-     * @param goalName the goal name of the log to parse
-     * @param log the goal log to parse
+     * @param goalName   the goal name of the log to parse
+     * @param log        the goal log to parse
      * @return the output of the parsed goal or {@code null} if no parser was found to parse the log of the specified
      * goal
      */
@@ -32,17 +32,45 @@ public class MavenGoalLogParser {
     }
 
     /**
+     * Tries to parse the log of a error message.
+     *
+     * @param errorMessage the error message
+     * @param log          the log of the error message (often just the build summary at the end of the maven build log)
+     * @return the output of the parsed error or {@code null} if no parser was found to parse the log
+     */
+    public static List<Error> parseErrorLog(String errorMessage, String log) {
+        return new MavenGoalLogParser().parse(errorMessage, log);
+    }
+
+    /**
      * Tries to parse the log of a goal with one of the available goal parsers.
      *
      * @param pluginName the plugin name of the log to parse
-     * @param goalName the goal name of the log to parse
-     * @param log the goal log to parse
+     * @param goalName   the goal name of the log to parse
+     * @param log        the goal log to parse
      * @return the output of the parsed goal or {@code null} if no parser was found to parse the log of the specified
      * goal
      */
     private List<Error> parse(String pluginName, String goalName, String log) {
         for (GoalParser parser : this.goalParserList) {
-            if (parser.isParserFor(pluginName, goalName)) {
+            if (parser.isParserForPlugin(pluginName, goalName)) {
+                return parser.parseLog(log);
+            }
+        }
+
+        return null;
+    }
+
+    /**
+     * Tries to parse the log of a error message.
+     *
+     * @param errorMessage the error message
+     * @param log          the log of the error message (often just the build summary at the end of the maven build log)
+     * @return the output of the parsed error or {@code null} if no parser was found to parse the log
+     */
+    private List<Error> parse(String errorMessage, String log) {
+        for (GoalParser parser : this.goalParserList) {
+            if (parser.isParserForErrorMessage(errorMessage)) {
                 return parser.parseLog(log);
             }
         }
