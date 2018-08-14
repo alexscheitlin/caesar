3c9277ac18d3da851f63a82381426e9ac520729f
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-14 11:43:16 +0200

    Refine and comment error-model test

--- error-model/src/test/java/ch/scheitlin/alex/build/model/ErrorTest.java
+++ error-model/src/test/java/ch/scheitlin/alex/build/model/ErrorTest.java
@@ -7,56 +7,76 @@ public class ErrorTest {
 
     @Test
     public void getPath() {
+        // assign variables with test data
         String expectedPath = "D:/some/path";
 
+        // allocate test objects
         Error error = new Error(expectedPath, null, 0, 0, null);
 
+        // execute method to be tested
         String actualPath = error.getPath();
 
+        // assert result
         Assert.assertEquals(expectedPath, actualPath);
     }
 
     @Test
     public void getFile() {
+        // assign variables with test data
         String expectedFile = "file.txt";
 
+        // allocate test objects
         Error error = new Error(null, expectedFile, 0, 0, null);
 
+        // execute method to be tested
         String actualFile = error.getFile();
 
+        // assert result
         Assert.assertEquals(expectedFile, actualFile);
     }
 
     @Test
     public void getLine() {
+        // assign variables with test data
         int expectedLine = 1;
 
+        // allocate test objects
         Error error = new Error(null, null, expectedLine, 0, null);
 
+        // execute method to be tested
         int actualLine = error.getLine();
 
+        // assert result
         Assert.assertEquals(expectedLine, actualLine);
     }
 
     @Test
     public void getColumn() {
+        // assign variables with test data
         int expectedColumn = 1;
 
+        // allocate test objects
         Error error = new Error(null, null, 0, expectedColumn, null);
 
+        // execute method to be tested
         int actualColumn = error.getColumn();
 
+        // assert result
         Assert.assertEquals(expectedColumn, actualColumn);
     }
 
     @Test
     public void getMessage() {
+        // assign variables with test data
         String expectedMessage = "message";
 
+        // allocate test objects
         Error error = new Error(null, null, 0, 0, expectedMessage);
 
+        // execute method to be tested
         String actualMessage = error.getMessage();
 
+        // assert result
         Assert.assertEquals(expectedMessage, actualMessage);
     }
 }
