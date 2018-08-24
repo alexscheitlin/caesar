6a2a3bd54a99d7d9f9d41c4c3985053c550fbf98
Alex Scheitlin <alex.scheitlin@bluewin.ch>
2018-08-24 14:20:18 +0200

    Refine and clean up main method to interact with CAESAR

--- caesar/src/main/java/Main.java
+++ caesar/src/main/java/Main.java
@@ -1,66 +1,115 @@
-import ch.scheitlin.alex.build.Assistant;
+import ch.scheitlin.alex.build.Caesar;
 import ch.scheitlin.alex.build.model.*;
 import ch.scheitlin.alex.build.model.Error;
+import ch.scheitlin.alex.maven.MavenBuildStatus;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        String host = "http://localhost";
-        String username = "alex";
-        String password = "scheitlin";
-        //String projectPath = "C:\\Users\\Alex\\IdeaProjects\\demo-bank-account";
-        String projectPath = "C:\\Users\\alex\\IdeaProjects\\demo-bank-account";
+    private static final BuildServerType BUILD_SERVER_TYPE = BuildServerType.TEAM_CITY;
+    private static final String HOST = "http://localhost";
+    private static final String USERNAME = "alex";
+    private static final String PASSWORD = "scheitlin";
 
-        // login to build server
-        BuildServerType type = BuildServerType.TEAM_CITY;
-        Assistant assistant = new Assistant(type);
-        if (!assistant.connect(host, username, password)) {
-            System.out.println("Could not connect to the TeamCity server.");
+    // the local path to the project to fix a broken build (to access the git repository)
+    private static final String PATH_TO_PROJECT = "C:\\Users\\alex\\IdeaProjects\\demo-bank-account";
+
+    // to run through the script without interacting with it
+    private static final boolean AUTO_RUN = false;
+    private static final int AUTO_RUN_PROJECT_INDEX = 1;
+    private static final int AUTO_RUN_BUILD_INDEX = 12;
+
+    public static void main(String[] args) {
+        System.out.println(getCaesarTitle());
+        Caesar caesar = new Caesar(BUILD_SERVER_TYPE);
+
+        // connect to build server
+        System.out.println();
+        System.out.println("Connecting to build server...");
+        if (!caesar.connect(HOST, USERNAME, PASSWORD)) {
+            System.out.println("\t failed");
+            exit();
             return;
         }
-        System.out.println("Status: " + (assistant.isConnected() ? "logged in" : "logged out"));
+        System.out.println("\tconnected");
 
         // get team city information about projects, build configurations, branches, and builds
-        BuildServer information = assistant.getBuildServerInformation();
+        System.out.println();
+        System.out.println("Fetch build server data...");
+        BuildServer buildServerData = caesar.getBuildServerInformation();
+        if (buildServerData == null) {
+            System.out.println("\t failed");
+            exit();
+            return;
+        }
+        System.out.println("\tfetched");
 
-        // list all projects configured on teamcity
-        List<String> projectNames = information.getProjectNames();
-        System.out.println("Projects:");
+        // list all projects configured on the build server
+        System.out.println();
+        List<String> projectNames = buildServerData.getProjectNames();
+        System.out.println("Configured projects on the build server:");
         for (int i = 0; i < projectNames.size(); i++) {
             System.out.println("\t" + "[" + (i + 1) + "] " + projectNames.get(i));
         }
 
-        // choose a project
-        System.out.print("Select a project by entering the corresponding number: ");
-        int projectNameIndex = 1;
-        System.out.println(projectNameIndex);
-        String projectName = projectNames.get(projectNameIndex - 1);
+        // select a project
+        int projectIndex = 0;
+        if (!AUTO_RUN) {
+            System.out.println();
+            System.out.print("Please select a project by entering the corresponding number: ");
+            try {
+                projectIndex = new Scanner(System.in).nextInt();
+            } catch (Exception ex) {
+                System.out.println("\tCould not read your input!");
+                exit();
+                return;
+            }
+        } else {
+            projectIndex = AUTO_RUN_PROJECT_INDEX;
+        }
+        String projectName = projectNames.get(projectIndex - 1);
+        BuildServerProject selectedProject = buildServerData.getProject(projectName);
 
-        // list all builds
-        BuildServerProject project = information.getProject(projectName);
+        // list all builds of the selected project
         System.out.println();
-        System.out.println(projectName);
+        System.out.println(selectedProject.getName());
         int counter = 0;
-        for (BuildServerBuildConfiguration config : project.getBuildConfigurations()) {
+        for (BuildServerBuildConfiguration config : selectedProject.getBuildConfigurations()) {
             System.out.println("\t" + config.getName());
 
             for (BuildServerBranch branch : config.getBranches()) {
                 System.out.println("\t\t" + (branch.getName() == null ? "default" : branch.getName()));
 
                 for (BuildServerBuild build : branch.getBuilds()) {
-                    System.out.println("\t\t\t" + "[" + ++counter + "] " + build.getNumber() + " - " + (build.getStatus() ? "success" : "failure"));
+                    System.out.println("\t\t\t" +
+                            String.format("%4s", "[" + ++counter + "]") + " " +
+                            (build.getStatus() ? "success" : "failure") +
+                            " (" + build.getStatusText() + ")"
+                    );
                 }
+                System.out.println();
             }
         }
 
-        // choose a build
-        System.out.print("Select a build by entering the corresponding number: ");
-        int buildIndex = 11;
-        System.out.println(buildIndex);
+        // select a build
+        int buildIndex = 0;
+        if (!AUTO_RUN) {
+            System.out.println();
+            System.out.print("Please select a build by entering the corresponding number: ");
+            try {
+                buildIndex = new Scanner(System.in).nextInt();
+            } catch (Exception ex) {
+                System.out.println("\tCould not read your input!");
+                exit();
+                return;
+            }
+        } else {
+            buildIndex = AUTO_RUN_BUILD_INDEX;
+        }
         BuildServerBuild selectedBuild = null;
         counter = 0;
-        for (BuildServerBuildConfiguration config : project.getBuildConfigurations()) {
+        for (BuildServerBuildConfiguration config : selectedProject.getBuildConfigurations()) {
             for (BuildServerBranch branch : config.getBranches()) {
                 for (BuildServerBuild build : branch.getBuilds()) {
                     if (++counter == buildIndex) {
@@ -69,7 +118,7 @@ public class Main {
                         System.out.println();
                         System.out.println("BuildServerBuild id:\t\t\t" + selectedBuild.getId());
                         System.out.println("BuildServerBuild number:\t\t" + selectedBuild.getNumber());
-                        System.out.println("BuildServerBuild status:\t\t" + (selectedBuild.getStatus()?"success":"failure"));
+                        System.out.println("BuildServerBuild status:\t\t" + (selectedBuild.getStatus() ? "success" : "failure"));
                         System.out.println("BuildServerBuild status text:\t" + selectedBuild.getStatusText());
                         System.out.println("Repository:\t\t\t\t\t\t" + selectedBuild.getRepository());
                         System.out.println("BuildServerBranch:\t\t\t\t" + selectedBuild.getBranch());
@@ -80,69 +129,130 @@ public class Main {
         }
 
         // download build log
-        if (!assistant.download(selectedBuild)) {
-            System.out.println("Could not download the build log of the specified build!");
+        System.out.println();
+        System.out.println("Downloading build log...");
+        if (!caesar.download(selectedBuild)) {
+            System.out.println("\t failed");
+            exit();
             return;
         }
+        System.out.println("\tdownloaded");
 
         // process build log
-        if (!assistant.process()) {
-            System.out.println("Could not process the downloaded build log!");
+        System.out.println();
+        System.out.println("Processing build log...");
+        if (!caesar.process()) {
+            System.out.println("\t failed");
+            exit();
+            return;
+        }
+        System.out.println("\tprocessed");
+
+        // show build information to the user
+        System.out.println();
+        System.out.println("Build Status:\t\t\t\t" + caesar.mavenBuild.getStatus());
+
+        if (caesar.mavenBuild.getStatus() == MavenBuildStatus.SUCCESS) {
+            exit();
             return;
         }
 
-        // show build information to the user
-        System.out.println("BuildServerBuild Status:\t" + assistant.mavenBuild.getStatus());
-        if (assistant.mavenBuild.getFailedGoal() != null) {
-            System.out.println("Failed Goal:\t\t\t\t" + assistant.mavenBuild.getFailedGoal());
+        System.out.print("Failed Goal:\t\t\t\t");
+        if (caesar.mavenBuild.getFailedGoal() != null) {
+            System.out.println(caesar.mavenBuild.getFailedGoal());
         } else {
-            System.out.println("Failed Goal:\t\t\t\t" + "No failed goal detected!");
+            System.out.println("No failed goal detected!");
         }
-        if (assistant.mavenBuild.getErrorMessage() != null) {
-            System.out.println("Error Message:\t\t\t\t" + assistant.mavenBuild.getErrorMessage());
+        System.out.print("Error Message:\t\t\t\t");
+        if (caesar.mavenBuild.getErrorMessage() != null) {
+            System.out.println(caesar.mavenBuild.getErrorMessage());
         } else {
-            System.out.println("Error Message:\t\t\t\t" + "No error message detected!");
+            System.out.println("No error message detected!");
         }
-        System.out.println("Failure Category:\t\t\t\t\"" + assistant.failureCategory);
-        List<Error> errors = assistant.errors;
+        System.out.println("Failure Category:\t\t\t" + caesar.failureCategory);
+
+        // show errors to the user
+        System.out.println();
+        List<Error> errors = caesar.errors;
         if (errors != null) {
             for (Error error : errors) {
-                System.out.println("Error:\t\t\t\t" + error.getMessage());
-                System.out.println("\tPath:\t\t\t" + error.getPath());
-                System.out.println("\tFile:\t\t\t" + error.getFile());
-                System.out.println("\tLine:\t\t\t" + error.getLine());
-                System.out.println("\tColumn:\t\t\t" + error.getColumn());
+                System.out.println("Error:\t\t" + error.getMessage());
+                System.out.println("\tPath:\t" + error.getPath());
+                System.out.println("\tFile:\t" + error.getFile());
+                System.out.println("\tLine:\t" + error.getLine());
+                System.out.println("\tColumn:\t" + error.getColumn());
             }
         }
 
-        // ask user to fix build
-        System.out.println("Do you want to checkout the code of the broken build and start fixing the error?");
-        System.out.println("Your uncommitted changes will be saved automatically!");
-        System.out.println("[1] Yes");
-        System.out.println("[2] No");
-        System.out.print("Select your answer: ");
+        System.out.println();
+        System.out.println("You may explore the errors in your local code. However, be aware that your local code may" +
+                " be different from the version that failed on the build server.");
 
-        int answer = 1;
-        System.out.println(answer);
+        System.out.println();
+        System.out.println("[1] View the code that caused the build break. Uncommitted changes will be stashed" +
+                " automatically.");
+        System.out.println("[2] Exit CAESAR.");
 
-        // fix build
-        if (answer == 1) {
-            if (!assistant.fix(projectPath)) {
+        System.out.println();
+        System.out.print("Please enter a number to continue: ");
+
+        // get choice
+        int choice = 0;
+        if (!AUTO_RUN) {
+            try {
+                choice = new Scanner(System.in).nextInt();
+            } catch (Exception ex) {
+                System.out.println("\tCould not read your input!");
+                exit();
                 return;
             }
-
-            // stop fixing build
-            System.out.println("Enter '1' once you are finished fixing the build.");
-            System.out.println("If there are saved and uncommitted changes they will be applied automatically.");
-            System.out.println("1");
-
-            assistant.finish();
+        } else {
+            exit();
+            return;
+        }
+        if (choice != 1) {
+            exit();
+            return;
         }
 
-
-        // logout from teamcity
-        assistant.disconnect();
+        // check out broken code
         System.out.println();
-        System.out.println("Status: " + (assistant.isConnected() ? "logged in" : "logged out"));
+        System.out.println("Loading broken code...");
+        if (!caesar.fix(PATH_TO_PROJECT)) {
+            System.out.println("\t failed");
+            exit();
+            return;
+        }
+        System.out.println("\tloaded");
+
+        System.out.println();
+        System.out.print("Please press enter after you are finished fixing the code.");
+
+        try {
+            new Scanner(System.in).nextLine();
+        } catch (Exception ex) {
+            System.out.println("\tCould not read your input!");
+        }
+
+        // logout from the build server
+        caesar.disconnect();
+        System.out.println();
+        System.out.println("Status: " + (caesar.isConnected() ? "logged in" : "logged out"));
+
+        exit();
+    }
+
+    private static void exit() {
+        System.out.println();
+        System.out.println("Exit Caesar!");
+    }
+
+    private static String getCaesarTitle() {
+        return "  _____          ______  _____         _____  \n" +
+                " / ____|   /\\   |  ____|/ ____|  /\\   |  __ \\ \n" +
+                "| |       /  \\  | |__  | (___   /  \\  | |__) |\n" +
+                "| |      / /\\ \\ |  __|  \\___ \\ / /\\ \\ |  _  / \n" +
+                "| |____ / ____ \\| |____ ____) / ____ \\| | \\ \\ \n" +
+                " \\_____/_/    \\_\\______|_____/_/    \\_\\_|  \\_\\";
     }
 }
